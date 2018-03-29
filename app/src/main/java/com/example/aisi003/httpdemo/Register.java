b48403db573a61e7928bd7a94e03cdb32523ab75
EDGSCOUT <shanlianlei0225@qq.com>
2018-03-29 16:55:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aisi003/httpdemo/Register.java
@@ -0,0 +1,18 @@
+package com.example.aisi003.httpdemo;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by aisi003 on 2018/3/12.
+ */
+
+public class Register extends  Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+    }
+}
